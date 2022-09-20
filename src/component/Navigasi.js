412cc9ea53d4cb8f2b1b90dc4a5bc8c2c0ba6ac6
wgsdani <daniwgs92@gmail.com>
2022-09-20 14:29:44 +0700

    React JS-2

---
+++ src/component/Navigasi.js
@@ -0,0 +1,18 @@
+// import Container from 'react-bootstrap/Container';
+
+// export default function Navbar() {
+//     return ( <Navbar bg="dark" variant="dark">
+//     <Container>
+//       <Navbar.Brand href="#home">
+//         <img
+//           alt=""
+//           src="/logo.svg"
+//           width="30"
+//           height="30"
+//           className="d-inline-block align-top"
+//         />{' '}
+//         React Bootstrap
+//       </Navbar.Brand>
+//     </Container>
+//   </Navbar>
+// )}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import Navbar from "./component/Navigasi";
+import Container from 'react-bootstrap/Container';
+import "bootstrap/dist/css/bootstrap.min.css";
+// import './index.css';
+import App from "./App"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App name={"Deem"} job={"peserta"}/>
+    <App name={"Dani"} job={"peserta"}/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// function renderDOM(content, id) {
+//   ReactDOM.render(content, document.getElementById(id))
+  
+// }
+
+// const element = <div style={{color:"black"}}>
+//   <Navigasi/>
+//   <Container className='Container'>
+//   <h1>Bootcamp WGS Batch 3</h1>
+//   <h2>Ronatio Parhorasan Silaban</h2>
+//   </Container>
+//   </div>;
+
+
+// renderDOM(element,"root")
