2dce2e2e4c771c2d3a3c4f301b5b633ea087f257
wgsdani <daniwgs92@gmail.com>
2022-09-26 09:42:15 +0700

    ReactJS-Add button likes

--- src/App.js
+++ src/App.js
@@ -7,48 +7,48 @@ import Comment from './components/Comment';
 
 //  Function
 
-// function App () {
-//     // const [likes, setLikes] = useState (0)
-//     return (
-//       <div className='ui container comments'>
-//          {
-//             data.map((item, index) => (
-//               <Comment key={index} item={item}/>
-//            ))
-//           }
-//          </div>
-//     );
-//   };
-
-
-
-// Class Component
-class App extends React.Component {
-  render () {
+function App () {
+    // const [likes, setLikes] = useState (0)
     return (
       <div className='ui container comments'>
          {
-            this.props.data?.map((item, index) => (
-        <div className='comment' key={index}>
-          <a href='/' className='avatar'>
-            <img alt='avatar' src={item.avatar}/>
-          </a>
-          <div className='content'>
-            <a href='/' className='author'>
-             {item.name}
-            </a>
-            <div className='metadata'>
-              <span className='date'>Today, {item.date}</span>
-            </div>
-            <div className='text'>{item.comment}</div>
-          </div>
-        </div>    
+            data.map((item, index) => (
+              <Comment key={index} item={item}/>
            ))
           }
          </div>
     );
-  }
-};
+  };
+
+
+
+// Class Component
+// class App extends React.Component {
+//   render () {
+//     return (
+//       <div className='ui container comments'>
+//          {
+//             this.props.data?.map((item, index) => (
+//         <div className='comment' key={index}>
+//           <a href='/' className='avatar'>
+//             <img alt='avatar' src={item.avatar}/>
+//           </a>
+//           <div className='content'>
+//             <a href='/' className='author'>
+//              {item.name}
+//             </a>
+//             <div className='metadata'>
+//               <span className='date'>Today, {item.date}</span>
+//             </div>
+//             <div className='text'>{item.comment}</div>
+//           </div>
+//         </div>    
+//            ))
+//           }
+//          </div>
+//     );
+//   }
+// };
 
 
 
